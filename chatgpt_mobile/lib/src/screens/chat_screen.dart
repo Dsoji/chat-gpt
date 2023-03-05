910b374784774ad9bb90bd0dc3b8bf81ffd65eee
Snow <dami.osoj@gmail.com>
2023-03-05 13:59:25 +0100

    feat: app complete

---
+++ chatgpt_mobile/lib/src/screens/chat_screen.dart
@@ -0,0 +1,145 @@
+import 'dart:developer';
+
+import 'package:chatgpt_mobile/src/constants/constants.dart';
+import 'package:chatgpt_mobile/src/models/chat_model.dart';
+import 'package:chatgpt_mobile/src/provider/models_provider.dart';
+import 'package:chatgpt_mobile/src/services/api_services.dart';
+import 'package:chatgpt_mobile/src/services/asset_manager.dart';
+import 'package:chatgpt_mobile/src/services/services.dart';
+import 'package:chatgpt_mobile/src/widgets/chat_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:provider/provider.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  bool _isTyping = false;
+
+  late TextEditingController textEditingController;
+  late FocusNode focusNode;
+  @override
+  void initState() {
+    textEditingController = TextEditingController();
+    focusNode = FocusNode();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  List<ChatModel> chatList = [];
+  @override
+  Widget build(BuildContext context) {
+    final modelsProvider = Provider.of<ModelsProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(AssetsManager.openaiLogo),
+        ),
+        title: const Text("ChatGPT"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await Services.showModalSheet(context: context);
+            },
+            icon: const Icon(Icons.more_vert_rounded, color: Colors.white),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                  itemCount: chatList.length,
+                  itemBuilder: (context, index) {
+                    return ChatWidget(
+                      msg: chatList[index].msg,
+                      chatIndex: chatList[index].chatIndex,
+                    );
+                  }),
+            ),
+            if (_isTyping) ...[
+              const SpinKitThreeBounce(
+                color: Colors.white,
+                size: 18,
+              ),
+            ],
+            const SizedBox(
+              height: 15,
+            ),
+            Material(
+              color: cardColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        focusNode: focusNode,
+                        style: const TextStyle(color: Colors.white),
+                        controller: textEditingController,
+                        onSubmitted: (value) async {
+                          await sendMessageFCT(
+                            modelsProvider: modelsProvider,
+                          );
+                        },
+                        decoration: const InputDecoration.collapsed(
+                            hintText: "How can I help you",
+                            hintStyle: TextStyle(color: Colors.grey)),
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () async {
+                          await sendMessageFCT(
+                            modelsProvider: modelsProvider,
+                          );
+                        },
+                        icon: const Icon(
+                          Icons.send,
+                          color: Colors.white,
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> sendMessageFCT({required ModelsProvider modelsProvider}) async {
+    try {
+      setState(() {
+        _isTyping = true;
+        chatList.add(ChatModel(msg: textEditingController.text, chatIndex: 0));
+        textEditingController.clear();
+        focusNode.unfocus();
+      });
+      chatList.addAll(await ApiService.sendMessage(
+        message: textEditingController.text,
+        modelId: modelsProvider.getCurrentModel,
+      ));
+      setState(() {});
+    } catch (error) {
+      log("error $error");
+    } finally {
+      setState(() {
+        _isTyping = false;
+      });
+    }
+  }
+}
